416b26ea13169c63ddfc5ca065072f5574aac73e
Jimmy <86750428+JmmyLi@users.noreply.github.com>
2022-10-20 08:27:27 +0800

    Add code segments
    More details

--- Cmd.java
+++ Cmd.java
@@ -2,18 +2,47 @@ public class Cmd {
     public static void run(String cmd){
         if(eq(cmd, "help")) help();
         else if(eq(cmd, "inventory")||eq(cmd, "i")) inventory();
+        else if(eq(cmd,"look")||eq(cmd, "l")) look();
+        else if(eq(cmd, "map")||eq(cmd, "m")) map();
+        else if(eq(cmd, "w")) w();
+        else if(eq(cmd, "s")) s();
+        else if(eq(cmd, "a")) a();
+        else if(eq(cmd, "d")) d();
     }
 
     private static void help(){
+        System.out.println("Basics:");
         System.out.println("Type W,A,S,D to navigate");
         System.out.println("Type 'inventory' or 'i' to open your bag");
         System.out.println("Type 'map' or 'm' to open your map");
         System.out.println("    -The map will only show the places you have passed.");
         System.out.println("Type 'look' or 'l' to look around");
+
+        System.out.println("Advanced commands(needs to be unlocked):");
+        System.out.println("W3 -> left three steps");
+
     }
 
     private static void inventory(){
-        
+
+    }
+    private static void look(){
+
+    }
+    private static void w(){
+
+    }
+    private static void s(){
+
+    }
+    private static void a(){
+
+    }
+    private static void d(){
+
+    }
+    private static void map(){
+
     }
     
     public static boolean eq(String str,String cmd) {return str.equalsIgnoreCase(cmd);}
--- Main.java
+++ Main.java
@@ -1,6 +1,11 @@
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.*;
 public class Main{
+    static String[] map = new String[22];
+    static int i,j,k;
     public static void main(String[] args){
+        getMap();
         welcome();
         Scanner s = new Scanner(System.in);
         String n = new String();
@@ -23,8 +28,23 @@ public class Main{
         System.out.println("Welcome to the Horre-4/3 Land!");
         System.out.println("type 'help' to learn more");
     }
-    public static void map(){
-        String MAP = new String("");
+    public static void getMap(){
+        // FileInputStream fin = new FileInputStream("map.txt");
+        InputStream fin = Main.class.getResourceAsStream("map.txt");
+        byte[] bytes = new byte[2];
+        int len = 0;i=0;
+        try {
+            while((len=fin.read(bytes)) != -1){
+                String pt = new String(bytes,0,len);
+                // System.out.print(pt);
+                if(pt=="\n") {System.out.println("map[i++]");}
+                else map[i]+=pt;
+            }
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        // System.out.println(inputStream);
     }
 }
 /*
